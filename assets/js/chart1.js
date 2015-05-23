40de5fa6e518e84278e2ccead75e8d4ab5a545b7
[kenichi.shibata31@gmail.com] <kenichi.shibata31@gmail.com>
2015-05-23 20:37:37 +0900

    edited everything

---
+++ assets/js/chart1.js
@@ -0,0 +1,8 @@
+
+$("#demo").jChart({
+name: "North American Regional Population Loss 2014",
+headers: ["Arizona","Michigan","Ontario","British Columbia","Texas"],
+values: [250000,478000,88000,429000,423000],
+footers: [100000,200000,300000,400000,500000],
+colors: ["#1000ff","#006eff","#00b6ff","#00fff6","#00ff90"]
+});<br>
